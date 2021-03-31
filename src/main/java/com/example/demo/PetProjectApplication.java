1646729767e7418320378ec1ac461955218d543b
KirillMedvedevSolbeg <kirill.medvedev@solbeg.com>
2021-03-31 14:52:03 +0300

    checked

--- src/main/java/com/example/demo/PetProjectApplication.java
+++ src/main/java/com/example/demo/PetProjectApplication.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PetProjectApplication {
-    //delete
+
 	public static void main(String[] args) {
-		SpringApplication.run(PetProjectApplication.class, args);
-	}
+        SpringApplication.run(PetProjectApplication.class, args);
+    }
 
 }
